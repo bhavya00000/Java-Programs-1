a44f28c726458453f3dba47a56ed2f47757fa949
bhavya00000 <112680819+bhavya00000@users.noreply.github.com>
2022-09-02 11:09:12 +0530

    Add files via upload

---
+++ Assignment_1/Abc.java
@@ -0,0 +1,9 @@
+public class Abc
+{
+public static void main(String args[])
+{
+     	System.out.println("1st Argument:"+args[0]);
+	System.out.println("2nd Argument:"+args[1]);
+	System.out.println("3rd Argument:"+args[2]);
+}
+}
